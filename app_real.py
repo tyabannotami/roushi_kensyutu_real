ef49aca5197c94b26de2c0e85febabeec8032579
tyabannotami <issi_kutuki2222@yahoo.co.jp>
2024-07-20 20:33:02 +0900

    もとに戻す

--- app_real.py
+++ app_real.py
@@ -7,22 +7,15 @@ from sahi.predict import get_prediction
 from sahi.utils.cv import visualize_object_predictions
 import av
 from ultralytics import YOLO
-import time
-import threading
 # YOLOv8の推論関数
-model = YOLO('models/best.pt')
 
-# 検出されたかどうかのフラグ
-detected_flag = [False]
+model = YOLO('models/best.pt')
 
 def callback(frame):
     img = frame.to_ndarray(format="bgr24")
 
     # YOLOv8を用いて検出を行う
     results = model(img)
-    
-    kensyutu_flag = False
-
     # 検出結果の画像を描画
     for result in results:
             for box in result.boxes:
@@ -32,20 +25,12 @@ def callback(frame):
                 label = f'{model.names[cls]} {conf:.2f}'
                 img = cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
                 img = cv2.putText(img, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-                kensyutu_flag=True
-
-   # 検出された場合にフラグを設定
-    if kensyutu_flag:
-        detected_flag[0] = True
 
     return av.VideoFrame.from_ndarray(img, format="bgr24")
-
 
 st.title('大熊老師探しリアルタイム検出')
 st.subheader('Webカメラを使ってリアルタイムで大熊老師を検出します。')
 
-# 音声ファイルを読み込む
-audio_file = open('bonavu.mp3', 'rb').read()
 
 # エラーハンドリングの追加
 try:
@@ -60,8 +45,3 @@ try:
     )
 except Exception as e:
     st.error(f"エラーが発生しました: {e}")
-
-# 検出フラグをチェックして音声再生
-if detected_flag[0]:
-    detected_flag[0] = False
-    st.audio(audio_file, format='audio/mp3')
